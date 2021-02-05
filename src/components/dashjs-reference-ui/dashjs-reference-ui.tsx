b6d35905ede39106c24151997a15496ce3750652
DanielHabenicht <daniel-habenicht@outlook.de>
2021-02-05 11:50:26 +0100

    Add more auto generation stuff for different versions  (#37)
    * add version select
    
    * add generator script for all version and overwrite functionality
    
    * add version selection

--- src/components/dashjs-reference-ui/dashjs-reference-ui.tsx
+++ src/components/dashjs-reference-ui/dashjs-reference-ui.tsx
@@ -7,10 +7,30 @@ import { Component, Host, h, Prop } from '@stencil/core';
 })
 export class DashjsReferenceUi {
   @Prop() url: string;
-
+  @Prop() versions: string[] = [];
+  @Prop() selectedVersion: string = '';
+  componentWillLoad() {
+    fetch('/static/gen/versions.json')
+      .then((response: Response) => response.json())
+      .then(response => {
+        this.versions = response;
+        this.selectedVersion = response[0];
+      });
+  }
   render() {
     return (
       <Host>
+        <ion-toolbar>
+          <ion-title>DashJS Reference UI</ion-title>
+          <ion-buttons slot="end">
+            Version:
+            <ion-select interface="popover" value={this.selectedVersion}>
+              {this.versions.map(version => (
+                <ion-select-option value={version}>{version}</ion-select-option>
+              ))}
+            </ion-select>
+          </ion-buttons>
+        </ion-toolbar>
         <dashjs-api-control></dashjs-api-control>
         <dashjs-settings-control onSettingsUpdated={event => console.log(event.detail)}></dashjs-settings-control>
         <dashjs-player></dashjs-player>
