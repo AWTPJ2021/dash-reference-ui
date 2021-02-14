4e71805e79274aa83d6637ae9abc7aafb2087ba2
Karim Kozman <kimo_emad@hotmail.com>
2021-02-14 16:48:36 +0100

    removed akamai initialtization

--- src/components/dashjs-player/dashjs-player.tsx
+++ src/components/dashjs-player/dashjs-player.tsx
@@ -1,7 +1,6 @@
 import { Component, Host, h, Element, State, Prop, Watch, Listen, Event, EventEmitter } from '@stencil/core';
 import { MediaPlayerClass } from 'dashjs';
 declare var dashjs: any;
-// declare var ControlBar: any;
 
 @Component({
   tag: 'dashjs-player',
@@ -43,8 +42,6 @@ export class DashjsPlayer {
         }
         this.player = dashjs.MediaPlayer().create();
         this.player.initialize(this.element.querySelector('#myMainVideoPlayer'), event.detail.url, event.detail.autoPlay == 'true');
-        // const controlbar = new ControlBar(this.player);
-        // controlbar.initialize();
         this.streamInterval = setInterval(() => {
           this.streamMetricsEventHandler(this.player);
         }, 1000);
@@ -99,17 +96,6 @@ export class DashjsPlayer {
   }
 
   componentDidLoad() {
-    // console.log(this.element);
-    // this.player = MediaPlayer().create();
-    // this.player.initialize(this.element.querySelector('#myMainVideoPlayer'), this.url, this.autoPlay);
-    // const controlbar = new ControlBar(this.player);
-    // controlbar.initialize();
-
-    // this.isPaused = this.player.isPaused();
-    //let url = this.currentUrl;
-    //let player = MediaPlayer().create();
-    //player.initialize(this.element.shadowRoot.querySelector('#myMainVideoPlayer'), url, true);
-
     this.loadOrUpdateDashJsScript();
   }
 
@@ -153,40 +139,6 @@ export class DashjsPlayer {
       <Host>
         <slot>
           <ion-card>
-            {/* <div class="myMainVideoPlayer" id="myMainVideoPlayer">
-              <video controls={true} id="myMainVideoPlayer"></video>
-              <div id="videoController" class="video-controller unselectable">
-                <div id="playPauseBtn" class="btn-play-pause" title="Play/Pause">
-                  <span id="iconPlayPause" class="icon-play"></span>
-                </div>
-                <span id="videoTime" class="time-display">
-                  00:00:00
-                </span>
-                <div id="fullscreenBtn" class="btn-fullscreen control-icon-layout" title="Fullscreen">
-                  <span class="icon-fullscreen-enter"></span>
-                </div>
-                <div id="bitrateListBtn" class="control-icon-layout" title="Bitrate List">
-                  <span class="icon-bitrate"></span>
-                </div>
-                <input type="range" id="volumebar" class="volumebar" value="1" min="0" max="1" step=".01" />
-                <div id="muteBtn" class="btn-mute control-icon-layout" title="Mute">
-                  <span id="iconMute" class="icon-mute-off"></span>
-                </div>
-                <div id="trackSwitchBtn" class="control-icon-layout" title="A/V Tracks">
-                  <span class="icon-tracks"></span>
-                </div>
-                <div id="captionBtn" class="btn-caption control-icon-layout" title="Closed Caption">
-                  <span class="icon-caption"></span>
-                </div>
-                <span id="videoDuration" class="duration-display">
-                  00:00:00
-                </span>
-                <div class="seekContainer">
-                  <input type="range" id="seekbar" value="0" class="seekbar" min="0" step="0.01" />
-                </div>
-              </div>
-            </div> */}
-
             <video controls={true} id="myMainVideoPlayer"></video>
           </ion-card>
         </slot>
