bf0837211ff6a1e90e086690d63fd8ad20a1e10a
DanielHabenicht <daniel-habenicht@outlook.de>
2021-03-04 22:25:33 +0000

    add tests and fix linting for help-button

--- src/components/dashjs-help-button/test/dashjs-help-button.e2e.ts
+++ src/components/dashjs-help-button/test/dashjs-help-button.e2e.ts
@@ -1,6 +1,6 @@
 import { newE2EPage } from '@stencil/core/testing';
 
-xdescribe('dashjs-help-button', () => {
+describe('dashjs-help-button', () => {
   it('renders', async () => {
     const page = await newE2EPage();
     await page.setContent('<dashjs-help-button></dashjs-help-button>');
